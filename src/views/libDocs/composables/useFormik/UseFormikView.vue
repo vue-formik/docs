bcc49b4e5cc7ef62e9dee33a5d014103cb0ed496
Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>
2025-01-11 22:44:18 +0545

    Improve content
    Signed-off-by: Kiran Parajuli <39373750+kiranparajuli589@users.noreply.github.com>

--- src/views/libDocs/composables/useFormik/UseFormikView.vue
+++ src/views/libDocs/composables/useFormik/UseFormikView.vue
@@ -18,157 +18,128 @@ import {
 
 <template>
   <section class="info_page">
-    <header>
-      <h1><code>useFormik</code> composable</h1>
+    <section>
+      <h1>✨ <code>useFormik</code> Composable</h1>
 
-      <p>
-        The useFormik composable is your go-to solution for managing complex forms in Vue 3
-        applications. With a focus on simplicity and efficiency, this composable provides reactive
-        state management, schema-based validation with support for libraries like yup, and
-        streamlined form submission handling. It enables developers to build dynamic, user-friendly
-        forms with ease, ensuring a robust and consistent user experience.
+      <p class="mb-2">
+        The <code>useFormik</code> composable is your ultimate ally for managing complex forms in
+        Vue 3 applications. With its simplicity and efficiency, it provides:
       </p>
 
-      <article>
-        <h4 class="flex items-center gap-2"><InfoIcon /> Performance:</h4>
+      <ul class="list-disc ml-6 mb-4">
+        <li class="mb-1">⚡ <strong>Reactive state management</strong></li>
+        <li class="mb-1">
+          ✅ <strong>Schema-based validation (supporting libraries like Yup)</strong>
+        </li>
+        <li>🚀 <strong>Streamlined form submission handling</strong></li>
+      </ul>
+      <p class="mb-1">
+        With <code>useFormik</code>, you can effortlessly create dynamic, user-friendly forms that
+        deliver a robust and consistent user experience.
+      </p>
+
+      <article class="!my-10">
+        <h4 title="performance" class="flex items-center gap-2"><InfoIcon /> Performance:</h4>
         <p>
-          Validation runs deeply on every change to values. Optimize schema and rules to avoid
-          unnecessary overhead.
+          Form validation runs deeply on every value change. To keep things snappy, optimize your
+          validation schema and rules.
         </p>
       </article>
-    </header>
+    </section>
 
     <section>
-      <h2>Parameters</h2>
+      <h2 title="parameters">🛠️ Parameters</h2>
       <SimpleTable :columns="UseFormikParameterColumns" :rows="UseFormikParameters" />
 
       <blockquote>
-        <p class="!text-md"><strong>Validation Schema:</strong></p>
+        <p class="!text-md"><strong>🧪 Validation Schema:</strong></p>
         <p>
           The <code>validationSchema</code> parameter supports both
-          <a href="https://yup-docs.vercel.app/">Yup</a>
-          schemas and custom validation rules.
+          <a href="https://yup-docs.vercel.app/">Yup</a> schemas and custom validation rules.
         </p>
         <p>
-          In the upcoming releases, we will be adding support for more validation libraries like
-          <a href="https://zod.dev/">Zod</a>, <a href="https://joi.dev/">Joi</a>, and more.
+          Coming soon: Support for more validation libraries like
+          <a href="https://zod.dev/">Zod</a> and <a href="https://joi.dev/">Joi</a>. Stay tuned! 🎉
         </p>
       </blockquote>
     </section>
 
     <section>
-      <h2>Properties</h2>
+      <h2 id="properties">🏗️ Properties</h2>
       <SimpleTable
         :columns="UseFormikReturnedPropertiesColumns"
         :rows="UseFormikReturnedProperties"
       />
 
       <blockquote>
-        <p class="!text-md"><strong>Notes:</strong></p>
+        <p class="!text-md"><strong>🔗 Notes:</strong></p>
         <ol class="list-decimal list-inside">
           <li>
-            <strong>Reactivity:</strong>
-            <span>
-              The values, errors, and touched objects are fully reactive and can be used for two-way
-              data binding with form fields.
-            </span>
+            <strong>Reactivity:</strong> Values, errors, and touched objects are fully reactive,
+            enabling two-way data binding with form fields.
           </li>
         </ol>
       </blockquote>
     </section>
 
     <section>
-      <h2>Methods</h2>
+      <h2 id="methods">⚙️ Methods</h2>
       <SimpleTable :columns="UseFormikReturnedMethodsColumns" :rows="UseFormikReturnedMethods" />
 
       <blockquote>
-        <p class="!text-md"><strong>Dynamic Fields:</strong></p>
+        <p class="!text-md"><strong>🌟 Dynamic Fields:</strong></p>
         <ol class="list-decimal list-inside">
+          <li><strong>setFieldValue:</strong> Dynamically update specific field values.</li>
           <li>
-            <strong>setFieldValue:</strong>
-            <span> This method allows dynamic updates to specific fields. </span>
+            <strong>setFieldTouched:</strong> Dynamically update the touched state of specific
+            fields.
           </li>
           <li>
-            <strong>setFieldTouched:</strong>
-            <span>
-              This method allows dynamic updates to the touched state of specific fields.
-            </span>
-          </li>
-          <li>
-            <strong class="text-red-100">setFieldError:</strong>
-            <span>
-              This method is <strong>NOT</strong> available. <strong>Why?</strong> Because it's
-              calculated internally and manually set values will not be effectively applied. We need
-              to write validation rules in the schema to handle errors.
-            </span>
+            <strong>⚠️ setFieldError:</strong> This method is <strong>NOT</strong> available. Errors
+            are handled internally through validation rules in the schema.
           </li>
         </ol>
       </blockquote>
     </section>
 
     <section>
-      <h2>Custom Validation</h2>
+      <h2 id="custom-validation">🔍 Custom Validation</h2>
       <p>
-        The <code>useFormik</code> composable supports custom validation rules. You can define
-        custom validation rules using the <code>validationSchema</code> parameter. The validation
-        schema can be a function that returns an object with validation rules for each
-        <strong>necessary</strong> field.
+        You can define custom validation rules using the <code>validationSchema</code> parameter.
+        These rules can validate simple fields, arrays, objects, or even arrays of objects.
       </p>
 
-      <h3>Example</h3>
+      <h3 id="example">Example</h3>
 
       <CodeBlock :content="CustomValidationObjectExample" />
 
       <p>
-        In the above example, we have defined a custom validation object that contains validation
-        rules for the <code>email</code> and <code>password</code> fields. The <code>email</code>{'
-        '} field is required and must be a valid email address. The <code>password</code> field is
-        required and must be at least 8 characters long.
+        The above example shows a validation schema for the <code>email</code> and
+        <code>password</code> fields. - **Email** must be valid and is required. - **Password** must
+        be at least 8 characters long and is required.
       </p>
       <p>
-        The <code>validationSchema</code> parameter accepts this object and uses it to validate the
-        form fields.
-      </p>
-      <p>
-        You can define custom validation rules for any field in the form. The validation schema can
-        contain rules for all the fields in the form or only for specific fields.
-      </p>
-      <p>
-        The custom validation object can should contain error messages that are displayed when the
-        validation fails.
+        Validation schemas can be customized to handle simple fields or more complex structures,
+        such as arrays or nested objects.
       </p>
 
-      <h4>My form values are not always strings</h4>
+      <h3 id="my-form-values-arent-always-strings" class="mt-8">
+        💡 My form values aren’t always strings
+      </h3>
       <p>
-        Don't worry! We got you covered. You can write validation rules to make complex schema like
-        <code>array</code>, <code>object</code> and <code>array of objects</code>.
+        No worries! Validation rules can be written for arrays, objects, or even arrays of objects.
       </p>
 
       <CodeBlock :content="CustomValidationWithEmailArrayExample" />
 
       <p>
-        In the above example, we have defined a custom validation object that contains validation
-        rules for the <code>emails</code> field. The <code>emails</code> field is an array of email
-        addresses. The validation schema validates each email in the array and displays an error
-        message if any of the emails are invalid.
+        In the example above, the <code>emails</code> field is an array of email addresses. Each
+        email is validated, and errors are displayed if any email is invalid.
       </p>
 
       <p>
-        Note that we returned two types of values. If the values array is empty, then string is
-        returned right away. If the values array is not empty, then we return an array of strings.
-        The library will understand the error message for each field by matching the index of the
-        values array.
-      </p>
-
-      <p>
-        This currently limits the error message to be a string only. We will be adding support for
-        returning an array of errors for each field in the upcoming releases.
-      </p>
-
-      <p>
-        More complex custom validation rules are documented in the
-        <router-link to="/playground">Playground</router-link> section. Please check it out for more
-        detailed overview.
+        For more complex validation rules, check out the
+        <router-link to="/playground">Playground</router-link> section. 🔗
       </p>
     </section>
   </section>
